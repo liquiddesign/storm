44a3b054df3fb7663a4b61e8fb2343e985fef8b8
Oldrich <oldrich@lqd.cz>
2021-03-25 11:02:15 +0100

    Expression

---
+++ src/Expression.php
@@ -0,0 +1,55 @@
+<?php
+
+declare(strict_types=1);
+
+namespace StORM;
+
+class Expression
+{
+	private int $iterator = 0;
+	
+	/**
+	 * @var mixed[]
+	 */
+	private array $vars = [];
+	
+	private string $sql = '';
+	
+	/**
+	 * @param string|null $glue
+	 * @param string $expression
+	 * @param mixed[] $vars
+	 */
+	public function add(?string $glue, string $expression, array $vars): void
+	{
+		$iterator = $this->iterator;
+		$stmVars = [];
+		
+		foreach ($vars as $i => $var) {
+			$stmVars[":__var$i" . "_$iterator"] = $var;
+		}
+		
+		$expression = \vsprintf($expression, \array_keys($stmVars));
+		
+		if ($this->sql && $glue) {
+			$this->sql .= " $glue ";
+		}
+		
+		$this->sql .= "($expression)";
+		$this->vars += $stmVars;
+		$this->iterator++;
+	}
+
+	public function getSql(): string
+	{
+		return $this->sql;
+	}
+	
+	/**
+	 * @return mixed[]
+	 */
+	public function getVars(): array
+	{
+		return $this->vars;
+	}
+}
